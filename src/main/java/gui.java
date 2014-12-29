3e73a5f789533688a3d14f964c53a1175c32e20a
Hugo Prado Figueiredo <hugo4566@gmail.com>
2014-12-29 10:39:04 -0300

    ftp java[2]

--- src/main/java/gui.java
+++ src/main/java/gui.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.awt.*;
 
 /**
  * Created by Hugo on 29/12/2014.
@@ -8,4 +9,24 @@ public class gui {
     private JTextField senhaField;
     private JTextField loginField;
     private JButton conectarButton;
+    private JPanel panel;
+    private JLabel statusLabel;
+
+    public gui() {
+        conectarButton.addActionListener(e -> {
+            System.out.print("OI");
+            statusLabel.setText("CONECTADO!");
+            statusLabel.setForeground(Color.GREEN);
+        });
+    }
+
+
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("gui");
+        frame.setContentPane(new gui().panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
 }
