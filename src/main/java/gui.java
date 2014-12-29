0652d9d99157fa4cb57a80848c18c17384068c5b
Hugo Prado Figueiredo <hugo4566@gmail.com>
2014-12-29 10:26:54 -0300

    ftp java

---
+++ src/main/java/gui.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+
+/**
+ * Created by Hugo on 29/12/2014.
+ */
+public class gui {
+    private JTextField serverField;
+    private JTextField senhaField;
+    private JTextField loginField;
+    private JButton conectarButton;
+}
